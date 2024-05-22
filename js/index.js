c1eb7fd12845845c282918e6282fd9ad3fe89aac
med <mohamedouahmane550@gmail.com>
2024-05-22 19:40:55 +0100

    Please commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//git commands
+// git init => starts a repo inside the current project.
+// git classifies the files in the project to two states:
+
+//Untracked files => not tracked by git (initial state)
+//Tracked files => tracked by git using (git add file-name) command
+
+//Tracked files have 3 states:
+
+//Unmodified: No changes were applied to the tracked file.
+
+//Modified: File has been changed using (git add file-name)
+
+//Staged:Before commit.
+
+//When file is committed, It will change state to unmodified again.
+
